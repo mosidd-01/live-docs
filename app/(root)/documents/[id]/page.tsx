c2c94642d82a4932bfae0b702b066490de52f451
Muhammad Siddiqui <s760132@gmail.com>
2024-08-05 04:37:32 -0500

    implement header

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,16 @@
+import Header from "@/components/Header";
 import { Editor } from "@/components/editor/Editor";
+import { PhoneCall } from "lucide-react";
 import React from "react";
 
 const Document = () => {
   return (
     <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">Test Title</p>
+        </div>
+      </Header>
       <Editor />
     </div>
   );
